6d5960d2d0121c00a9d36fa9fad5fefb3616bc5d
Maurine <maurinek04@gmail.com>
2023-11-01 16:16:43 +0000

    checks if singly linked has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if linked list has a cycle
+ * @list: pointer representing the head
+ *
+ * Return: returns 0 if no cycle and 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first = list;
+	listint_t *second = list;
+
+	while (first != NULL)
+	{
+		second = second->next;
+		first = first->next->next;
+		if (second == first)
+			return (1);
+	}
+	return (0);
+}
