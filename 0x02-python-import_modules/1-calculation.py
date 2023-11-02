7eeea3ae77d6c2936386e73d888ff6c0d2429197
Maurine <maurinek04@gmail.com>
2023-11-02 20:08:05 +0000

    imports functions, performs maths and prints out the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,15 +2,8 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-    from calculator_1 import add
-    result = add(a, b)
-    print('{} + {} = {}'.format(a, b, result))
-    from calculator_1 import sub
-    result = sub(a, b)
-    print('{} - {} = {}'.format(a, b, result))
-    from calculator_1 import mul
-    result = mul(a, b)
-    print('{} * {} = {}'.format(a, b, result))
-    from calculator_1 import div
-    result = div(a, b)
-    print('{} / {} = {}'.format(a, b, result))
+    from calculator_1 import add, sub, mul, div
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{} * {} = {}'.format(a, b, mul(a, b)))
+    print('{} / {} = {}'.format(a, b, div(a, b)))
