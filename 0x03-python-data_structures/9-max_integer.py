e6e1444b59f324347dc8259fefb3fc87c6e8259c
Maurine <maurinek04@gmail.com>
2023-11-07 01:14:06 +0000

    finds biggest integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bib/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_num = my_list[0]
+    for number in my_list:
+        if number > max_num:
+            max_num = number
+
+    return max_num
+
