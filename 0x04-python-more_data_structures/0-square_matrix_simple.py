8cf8f1064555453ddb4221b145f8680a2a503803
Maurine <maurinek04@gmail.com>
2023-11-08 21:47:54 +0000

    computes square values of all integers in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-new_matrix = []
-for row in matrix:
-    new_row = [item * item for item in row]
-    new_matrix.append(new_row)
-return new_matrix
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = [item * item for item in row]
+        new_matrix.append(new_row)
+    return new_matrix
