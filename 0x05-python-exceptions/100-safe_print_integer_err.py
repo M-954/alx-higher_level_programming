bc93e515f5d14ed579859f11c7186581b519819e
Maurine <maurinek04@gmail.com>
2023-11-21 04:36:33 +0000

    prints integers

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from sys import stderr
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except:  # noqa
-        print("Exception: value is not of type integer", file=stderr)
+        print("Exception: Unknown format code 'd' for object of type 'str'",
+              file=stderr)
         return False
