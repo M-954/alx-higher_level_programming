b4e420c42e59eb53aafce3c1b8a72deed9e8fbcb
Maurine <maurinek04@gmail.com>
2023-11-21 21:14:29 +0000

    initializes square with private length

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A project on classes and objects"""
+
+class Square:
+    """A class defined as square"""
+
+    def __init__(self, size):
+        """initializing the square and giving it a private length"""
+
+        
+        self.__size = size
