49673c35a74181aaab233f3d70a45604cbe84998
Maurine <maurinek04@gmail.com>
2023-11-22 03:00:21 +0000

    class that calculates area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A project onclasses and object"""
+
+
+class Square:
+    """A square defined class
+
+
+    Attributes:
+        __size: length of one side of square
+
+
+    raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than zero
+
+
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self):
+
+
+        """
+        calculates the area of a square
+
+
+        Attributes:
+        __size: length of one side of square
+
+
+        Returns: area of square
+
+
+        """
+        area = self.__size ** 2
+        return (area)
