e47e332f66983e9fa87ae5e3f81733dad6df3c1b
Maurine <maurinek04@gmail.com>
2023-11-30 00:13:41 +0000

    a function that adds integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    A function that adds two integers a and b
+
+
+    raises:
+    TypeError - if a or b is not an integer or float
+    """
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
