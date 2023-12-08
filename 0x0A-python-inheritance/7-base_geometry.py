af19e8fb806d478f06abcb1771be593b62e075ef
Maurine <maurinek04@gmail.com>
2023-12-08 09:08:46 +0000

    validates valuue

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+'''
+A 6-base_geometry module
+'''
+
+
+class BaseGeometry:
+    '''
+    A class with diffrent methods
+
+    raises:
+    Exception - if method is implemented
+    '''
+    def area(self):
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        '''
+        An integer validator method
+
+
+        attributes:
+        name - name given to the value
+        value - value given to represent name
+
+        raises:
+        TypeError - if value is not an integer
+        ValueError - if value is less than or equal to zero'''
+
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
