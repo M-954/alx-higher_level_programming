a44fc4afdb139961867fbe9b0f01c084f6b57131
Maurine <maurinek04@gmail.com>
2023-12-08 11:58:29 +0000

    creates a class rectangle that inherits from the basegeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''
+A 8-rectangle module
+'''
+
+
+class BaseGeometry:
+    '''
+    A class with diffrent methods
+
+    raises:
+    Exception - if method is implemented
+    '''
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''
+        An integer validator method
+
+
+        attributes:
+        name - name given to the value
+        value - value given to represent name
+
+        raises:
+        TypeError - if value is not an integer
+        ValueError - if value is less than or equal to zero'''
+
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+        if not name and not value:
+            raise TypeError("integer_validator() missing 2 required "
+                            "positional arguments: 'name' and 'value'")
+        if value is None:
+            raise TypeError("integer_validator() missing 1 required "
+                            "positional argument: 'value'")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
